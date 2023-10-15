c7c687c5841a4fe2630c08f602caa716ffc9607f
RepoRascal <126687635+RepoRascal@users.noreply.github.com>
2023-10-15 11:44:53 +0530

    Update upload.py

--- upload.py
+++ upload.py
@@ -1,30 +1,52 @@
 #!/bin/bash
 
-CHUNK_SIZE=95000000  # 95MB
+TARGET_SIZE=${TARGET_SIZE:-100000000}  # 100MB default, can be overridden with CircleCI env var
+CHUNK_SIZE=${CHUNK_SIZE:-95000000}    # slightly less than 100MB default
+REPO_DIR="~/project"  # default CircleCI checkout dir
+FILE_TO_MONITOR="ptr.txt"
+TEMP_FILE="temp_ptr.txt"
+LAST_PUSHED_SIZE=0
+LOG_FILE="upload_log.txt"
 
-# CircleCI will check out your repository to the folder "~/project"
-# So, your repository content will be in that directory.
-REPO_DIR="~/project"
+function log_message {
+    echo "$(date +%Y-%m-%d\ %H:%M:%S) - $1" | tee -a $LOG_FILE
+}
 
-FILE="${REPO_DIR}/ptr.txt"
+# Git setup for SSH (if required)
+# Assuming you've added the private key as an environment variable PRIVATE_SSH_KEY in CircleCI
+echo "$PRIVATE_SSH_KEY" > /root/id_rsa
+chmod 600 /root/id_rsa
+eval $(ssh-agent -s)
+ssh-add /root/id_rsa
+# Ensure the key is added without passphrase for automation.
 
-# Identify the last line of the first uploaded chunk.
-LAST_LINE=$(tail -n 1 "${REPO_DIR}/path/to/the/first/uploaded/chunk")
-
-# Find the line number of that line in ptr.txt.
-LINE_NUMBER=$(grep -nF "$LAST_LINE" "$FILE" | cut -d: -f1)
-
-# Extract everything up to that line and save to a temporary chunk.
-head -n "$LINE_NUMBER" "$FILE" > "${REPO_DIR}/initial_chunk"
-
-# Split the initial_chunk into smaller files of 95MB each.
+# Initialize a new git repository and set the remote
 cd "$REPO_DIR"
-split -b "$CHUNK_SIZE" -d --additional-suffix=.txt initial_chunk older_chunk_
+git init
+git remote add origin https://github.com/RepoRascal/Scan.git
 
-# Now, commit and push the older chunks.
-git add older_chunk_*
-git commit -m "Add older chunks of ptr.txt"
-git push origin main
+while true; do
+  # Using rsync to copy only the new content
+  rsync --inplace --size-only --append "$FILE_TO_MONITOR" "$TEMP_FILE"
 
-# Clean up the initial_chunk.
-rm initial_chunk
+  CURRENT_SIZE=$(stat -c%s "$TEMP_FILE")
+
+  # Check if the file size minus what we last pushed is greater than our target
+  if [[ $((CURRENT_SIZE - LAST_PUSHED_SIZE)) -ge $TARGET_SIZE ]]; then
+    CHUNK_NAME="ptr_chunk_$(date +%Y%m%d%H%M%S)"
+    tail -c +$((LAST_PUSHED_SIZE + 1)) "$TEMP_FILE" | head -c $CHUNK_SIZE > "${REPO_DIR}/$CHUNK_NAME"
+
+    LAST_PUSHED_SIZE=$((LAST_PUSHED_SIZE + CHUNK_SIZE))
+
+    # Now commit and push
+    git add "$CHUNK_NAME"
+    git commit -m "Add new chunk of ptr.txt"
+    if git push origin main; then
+        log_message "Successfully pushed $CHUNK_NAME to GitHub."
+    else
+        log_message "Failed to push $CHUNK_NAME to GitHub!"
+    fi
+  fi
+
+  sleep 60  # Wait for a minute before checking again
+done
