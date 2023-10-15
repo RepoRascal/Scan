187c44e343b7da98773beb1edfe342d1c1dc7b7f
RepoRascal <126687635+RepoRascal@users.noreply.github.com>
2023-10-15 12:02:17 +0530

    Update upload.py

--- upload.py
+++ upload.py
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-TARGET_SIZE=${TARGET_SIZE:-100000000}  # 100MB default, can be overridden with CircleCI env var
-CHUNK_SIZE=${CHUNK_SIZE:-95000000}    # slightly less than 100MB default
-REPO_DIR="~/project"  # default CircleCI checkout dir
+import os
+
+TARGET_SIZE = int(os.environ.get('TARGET_SIZE', '100000000'))  # 100MB default
+CHUNK_SIZE = int(os.environ.get('CHUNK_SIZE', '95000000'))    # slightly less than 100MB default
+REPO_DIR = os.environ.get('REPO_DIR', '~/project')  # default CircleCI checkout dir
 FILE_TO_MONITOR="ptr.txt"
 TEMP_FILE="temp_ptr.txt"
 LAST_PUSHED_SIZE=0
